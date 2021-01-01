8ded1a7b0fef3d85b0d05d11fad7eae1b0399e54
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2021-01-01 11:47:54 -0600

    makes and appends the axis functions.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,27 @@
 // @TODO: YOUR CODE HERE!
-var svgWidth = 960;
-var svgHeight = 660;
+var svgWidth = 500;
+var svgHeight = 500;
 var chartMargin = {
     top: 30,
     right: 30,
     bottom: 30,
     left: 30
     };
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+var width = svgWidth - chartMargin.left - chartMargin.right;
+var height = svgHeight - chartMargin.top - chartMargin.bottom;
 var svg = d3.select("#scatter").append("svg").attr("height", svgHeight).attr("width", svgWidth);
 var chartGroup = svg.append("g").attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
 
 d3.csv("assets/data/data.csv").then((data)=> {
     console.log(data);
-    obData.forEach(function (data){
+    data.forEach(function (data){
         data.income = +data.income
         data.obesity = +data.obesity
     })
+    
     var xLinearScale = d3.scaleLinear()
     .domain([0, d3.max(
-      obData.map(function (d) { 
+      data.map(function (d) { 
         return d.income;
       })
     )]
@@ -28,13 +29,22 @@ d3.csv("assets/data/data.csv").then((data)=> {
     .range([0, width]);
 
     var yLinearScale = d3.scaleLinear()
-    .domain([0, d3.max(obData.map(
+    .domain([0, d3.max(data.map(
       function (d) {
         return d.obesity;
       }
     ))]
     )
     .range([height, 0]);
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
+    chartGroup.append("g").attr("transform", `translate(0, ${height})`).call(bottomAxis);
+
+    chartGroup.append("g").call(leftAxis);
+
+
+
+
 
 
 
