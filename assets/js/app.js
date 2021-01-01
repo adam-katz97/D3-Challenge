c8e992258d25af148141cbb369c1e24788bed546
Adam Katz <adamkatz@Adams-MacBook-Pro-7.local>
2021-01-01 11:23:30 -0600

    rearranges code to be more readable.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,17 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 660;
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+    };
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+var svg = d3.select("#scatter").append("svg").attr("height", svgHeight).attr("width", svgWidth);
+var chartGroup = svg.append("g").attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
 d3.csv("assets/data/data.csv").then((data)=> {
     console.log(data.length);
     
@@ -9,23 +22,6 @@ d3.csv("assets/data/data.csv").then((data)=> {
   
 
 function show(data){
-    var svg = d3.select("#scatter").append("svg")
-    var svgWidth = 960;
-    var svgHeight = 660;
-    var chartMargin = {
-        top: 30,
-        right: 30,
-        bottom: 30,
-        left: 30
-      };
-      var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-      var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-      
-      
-      
-
-    
-
     var inc = [];
     var ob = []; 
     abv=[]
